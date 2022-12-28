2a387772a3c73ddf29885f05f4c85f7c06981694
Drew Vo <drewvo@Jimmys-Air.socal.rr.com>
2022-12-27 16:01:46 -0800

    close out

---
+++ css/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>100-control-flow</title>
+		
+		<!-- favicon -->
+		<!-- <link rel="icon" href="[[Insert favicon image source]]" type="image/x-icon"> -->
+		
+		<!-- style sheet -->
+		<!-- <link rel="stylesheet" href="css/style.css"> -->
+
+		<!-- metadata -->
+		<!-- <meta name="description" content="[[Insert description]]">
+
+		<meta property="og:image" content="[[Insert meta image source]]">
+		<meta property="og:image:secure_url" content="[[Insert meta image source]]">
+		<meta property="og:image:type" content="image/jpeg">
+		<meta property="og:image:width" content="1200">
+		<meta property="og:image:height" content="630">
+		<meta property="og:image:alt" content="[[Insert meta image description]]"> -->
+		
+	</head>
+
+	<body>
+		
+		<?php
+
+		$starting_price = ""; //in USD
+		$price_diff_type = "dollar_diff"; //dollar_diff or percentage_diff
+		$alerttype =
+		$calc_target_price = "";
+		$targetpercentage = "";
+
+		?>
+	</body>
