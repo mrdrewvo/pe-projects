b9eeae301ed0f42fe96898cf8b6a8cc2145f342f
Drew Vo <andrew.d.vo@gmail.com>
2023-01-09 22:12:50 -0500

    daily journal and php exploration exercise

---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Drew Vo</title>
+	
+	<!-- favicon -->
+	<link rel="icon" href="http://peprojects.dev/alpha-6/drew/images/logo.svg" type="image/x-icon">
+	
+	<!-- style sheets -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- metadata -->
+	<meta name="description" content="Hi, I'm Drew Vo - UX Engineer in training. Check out my learning journey as I set my right-brain free.">
+	<meta property="og:image" content="https://peprojects.dev/alpha-6/drew/images/logo-meta.jpg">
+	<meta property="og:image:secure_url" content="https://peprojects.dev/alpha-6/drew/images/logo-meta.jpg">
+	<meta property="og:image:type" content="image/jpeg">
+	<meta property="og:image:width" content="1200">
+	<meta property="og:image:height" content="630">
+	<meta property="og:image:alt" content="A blob-style graphic of letters D and V">
+	
+</head>
+
+<body>
+
+	<header class="page-section site-header top">
+
+		<inner-column>
+			
+			<nav>
+				<a href="#about-me">Let's vibe</a>
+				<a href="#featured-projects">Look what I made</a>
+				<a href="#resume">I did these</a>
+				<a href="#goals">I wanna do this</a>
+				<a href="#style-guide">How I wanna do it</a>
+			</nav>
+		</inner-column>
+	</header>
+
+	<main class="page-section site-main">
