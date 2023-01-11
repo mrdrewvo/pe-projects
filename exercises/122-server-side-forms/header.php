01579d305fc7e2a0289b557d577df9f3ac2669a7
Drew Vo <andrew.d.vo@gmail.com>
2023-01-11 15:30:48 -0500

    forms exercise and splitting up personal site

---
+++ exercises/122-server-side-forms/header.php
@@ -0,0 +1,9 @@
+
+<header class="page-section site-header">
+
+	<inner-column>
+		
+		<h1>Lesson 100 - PHP Control Flow</h1>
+
+	</inner-column>
+</header>
---
+++ exercises/122-server-side-forms/index.php
@@ -0,0 +1,110 @@
+<?php include('head.php'); ?>
+
+<?php include('header.php'); ?>
+		
+<main class="page-section site-main">
+
+	<section class="lesson-notes">
+	
+		<inner-column>
+
+			<h2>Lesson Notes</h2>
+
+			<form>
+				
+				<input type='text'/>
+				<!--self-closing tag, can have different types (e.g. text (default), number) -->
+
+				<input type='number'/>
+
+				<input type='email'/>
+
+				<input type='password'/>
+
+				<input type="range">
+
+				<button>button</button>
+				
+				<input type="submit">
+
+
+			</form>
+
+		</inner-column>
+	</section>
+
+
+
+
+
+
+
+
+
+
+
+	<section class="early-php-exploration">
+
+		<inner-column>
+
+			<h2>Ivy's Very Berry Party</h2>
+
+			<?php
+
+			$guestCount = 4;
+			$totalPeople = $guestCount + 1; // including our host, Ivy
+			$berriesPerPerson = 15;
+
+			$totalBerries = $totalPeople * $berriesPerPerson;
+
+			$message = "<p>We'll need $berriesPerPerson per guest. There will be $guestCount guests, but don't forget Ivy! So that means we need to buy $totalBerries berries in total for the event.</p>";
+
+			echo $message;
+
+			/* Extra Challenge (make sure to round):
+
+				Now add these - 
+				- 26 berries per box
+				- How many boxes will you need?
+				- What is the price per box?
+				- What is the price total?
+				
+			TDD:
+
+			Given the number of berries per box and the price per box, what is the total price to buy enough boxes of berries?
+
+			Inputs:
+			- $berriesPerBox: 10
+			- $pricePerBox: 7 (in USD)
+
+			Expected Results:
+			- $boxCount: 8 (7.5 but needs to round up)
+			- $totalPrice: $56 (need to print the '$')
+			
+			Pseudocode:
+			
+			Initalize $berriesPerBox as coded (OOS: validate for positive whole number)
+			Intitiailze $pricePerBox (in USD) as coded (OOS: round to .00)
+			
+			$boxCount = round(($totalBerries / $berriesPerBox), 0);
+			$totalPrice = $pricePerBox * $boxCount;
+
+			Display "Boxes each come with $berriesPerBox so we'll need to buy $boxCount boxes. Each box is $($pricePerBox) so the total budget needs to cover $totalPrice."
+
+			*/
+
+			$berriesPerBox = 26;
+			$pricePerBox = 7; // in USD
+
+			$boxCount = round(($totalBerries / $berriesPerBox), 0);
+			$totalPrice = $pricePerBox * $boxCount;
+
+			echo "<p>Boxes each come with $berriesPerBox so we'll need to buy $boxCount boxes. Each box is \${$pricePerBox} so the total budget needs to cover \${$totalPrice}.</p>";
+			?>
+
+		</inner-column>
+	</section>
+
+</main>
+
+<?php include('footer.php'); ?>
