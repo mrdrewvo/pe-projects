500218e05099f1248b7deebea510bf976874485a
Drew Vo <andrew.d.vo@gmail.com>
2023-01-13 00:42:20 -0500

    personal site back to new format

--- index.php
+++ index.php
@@ -1,17 +1,27 @@
 <?php include("head.php"); ?>
-	
+
+<header class="index-page">
+
+	<inner-column>
+		
+		<h1>Drew Vo</h1>
+
+		<tagline>
+			
+			<p>The butcher,</p>
+			
+			<p>the baker,</p>
+			
+			<p>the web app maker.</p>
+		</tagline>
+	</inner-column>
+</header>
+
 <main class="page-section site-main">
 	
-	<section class="home"> 
+	<section class="index-page"> 
 
 		<inner-column>
-
-
-				<h1>Drew Vo</h1>
-
-				<p>The butcher,</p>
-				<p>the baker,</p>
-				<p>the web app maker.</p>
 
 			<welcome-mod>
 
@@ -32,86 +42,6 @@
 				    </g>
 				</svg>
 			</welcome-mod>
-		</inner-column>
-	</section>
-
-	<section class="resume" id="resume">
-		
-		<inner-column>
-
-			<resume-mod class="main-section">
-
-				<resume-title>
-
-					<h2>Resume</h2>
-					<h2>I did this!</h2>
-				</resume-title>
-
-				<ul class="resume-list">
-						
-					<li class="company">
-						
-						<h3>Thumbtack</h3>
-						<role>
-							
-							<h4>Senior Product Operations Manager</h4>
-							<resume-date>Feb 2022 - Dec 2022</resume-date>	
-						</role>
-					</li>
-						
-					<li class="company">
-						
-						<h3>Verte</h3>
-						<role>
-							
-							<h4>Senior Manager, Customer Success</h4>
-							<resume-date>Aug 2021 - Feb 2022</resume-date>
-						</role>
-					</li>
-
-					<li class="company">
-						
-						<h3>Flexport</h3>
-						<role>
-							
-							<h4>Program Manager, Operational Excellence</h4>
-							<resume-date>Aug 2020 - Aug 2021</resume-date>
-						</role>
-						<role>
-							
-							<h4>Global Operations Manager</h4>
-							<resume-date>Sep 2019 - Aug 2020</resume-date>
-						</role>
-						<role>
-							
-							<h4>Senior Global Operations Associate</h4>
-							<resume-date>Feb 2018 - Sep 2019</resume-date>
-						</role>
-						<role>
-							
-							<h4>Global Operations Associate</h4>
-							<resume-date>Mar 2017 - Feb 2018</resume-date>
-						</role>
-					</li>
-
-					<li class="company">
-						
-						<h3>Shire</h3>
-						<role>
-							
-							<h4>Project Manager</h4>
-							<resume-date>Oct 2015 - Mar 2017</resume-date>
-						</role>
-						<role>
-							
-							<h4>Operations Leadership Development Program Associate</h4>
-							<resume-date>Jul 2013 - Oct 2015</resume-date>
-						</role>
-					</li>
-				</ul>
-
-				<?=$upArrow?>
-			</resume-mod>				
 		</inner-column>
 	</section>
 </main>
