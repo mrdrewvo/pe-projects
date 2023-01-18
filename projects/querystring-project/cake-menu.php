39efe8c6cc1c0f7f73630a6d6ca8f1301fb0eab5
Drew Vo <andrew.d.vo@gmail.com>
2023-01-18 18:44:03 -0500

    querystring project styling

--- projects/querystring-project/cake-menu.php
+++ projects/querystring-project/cake-menu.php
@@ -4,15 +4,18 @@
 
 
 <ul>
-	<?php foreach ($cakeData as $cake) { ?>
-		<li class="cake">
-		<cake-card>
-			<picture><img src=<?=$cake["pictureURL"]?> alt=""></picture>
-			<h2 class="name"><?=$cake["name"]?></h2>
-			<h3 class="date"><?=$cake["date"]?></h3>
-			<a href='?page=Cake Detail&cake=<?=$cake["id"]?>'>More Details</a>
-		</cake-card>
-	</li>
 
+	<?php foreach ($cakeData as $cake) { ?>
+
+		<li class="cake">
+
+			<cake-card>
+
+				<picture><img src=<?=$cake["pictureURL"]?> alt=""></picture>
+				<h2 class="name"><?=$cake["name"]?></h2>
+				<h3 class="date"><?=$cake["date"]?></h3>
+				<a href='?page=Cake Detail&cake=<?=$cake["id"]?>'>More Details</a>
+			</cake-card>
+		</li>
 	<?php } ?>
 </ul>
--- projects/querystring-project/home.php
+++ projects/querystring-project/home.php
@@ -1,1 +1,36 @@
-home.php
+<about>
+
+	<h1>Welcome to our humble bakeshop!</h1>
+
+	<p>Started by hobby baker, Drew Vo, Sterling Circle is a boutique bakeshop focused on custom modern-style cakes and macarons.</p>
+
+	<p>Named after the street where it all began in 2018, Sterling Circle Bakeshop has created hundreds of special orders for our customers.</p>
+</about>
+
+<?php include("cake-data.php"); ?>
+
+<?php foreach($cakeData as $cake) {
+		
+	if($cake["id"] == 10) { ?>
+
+		<cake-feature-card>
+			
+			<h1>Our latest creation</h1>
+
+			<cake-feature>
+				
+				<picture><img src=<?=$cake["pictureURL"]?> alt=""></picture>
+
+				<cake-feature-text>	
+					
+					<h2>"<?=$cake["name"]?>"</h2>
+
+					<p><span><?=$cake["date"]?></span> - <?=$cake["story"]?></p>
+
+					<a href='?page=Cake Detail&cake=<?=$cake["id"]?>'>More Details</a>
+
+				</cake-feature-text>
+			</cake-feature>
+		</cake-feature-card>
+	<?php };
+}; ?>
