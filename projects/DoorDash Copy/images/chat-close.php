63bc4a543036a77acda7e820ff09278f7bc0bf18
Drew Vo <andrew.d.vo@gmail.com>
2023-01-29 23:23:37 -0500

    doordashchat button

---
+++ projects/DoorDash Copy/images/chat-close.php
@@ -0,0 +1,19 @@
+<svg id="chat-close" width="100%" height="100%" viewBox="0 0 481 481" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+    <g transform="matrix(1,0,0,1,-2153.57,-48.4687)">
+        <g class="close-circle" transform="matrix(1,0,0,1,796.462,41.305)">
+            <g transform="matrix(1,0,0,1,26.5602,-53.4683)">
+                <circle cx="1571.01" cy="301.099" r="240.467" style="fill:rgb(35,48,105);"/>
+            </g>
+        </g>
+        <g class="close-x" transform="matrix(1,0,0,1,796.462,41.305)">
+            <g transform="matrix(1.24463,0,0,1.18039,-395.385,-30.7239)">
+                <g transform="matrix(1.23873,0,0,1.23873,-1311.51,-91.7742)">
+                    <path d="M2260.24,173.286C2383.33,296.375 2442.58,355.629 2442.58,355.629" style="fill:none;stroke:black;stroke-width:7.99px;"/>
+                </g>
+                <g transform="matrix(7.585e-17,1.23873,-1.23873,7.585e-17,1928.84,-2676.94)">
+                    <path d="M2260.24,173.286C2383.33,296.375 2442.58,355.629 2442.58,355.629" style="fill:none;stroke:black;stroke-width:7.99px;"/>
+                </g>
+            </g>
+        </g>
+    </g>
+</svg>
--- projects/DoorDash Copy/index.php
+++ projects/DoorDash Copy/index.php
@@ -22,8 +22,8 @@
 				
 				<inner-column>
 					
-					<?php include("modules/article-grid.php"); ?>					
-				</inner-column>	
+					<?php include("modules/article-grid.php"); ?>	
+				</inner-column>
 			</section>
 
 			<section class="bottom-banner">
@@ -42,5 +42,35 @@
 		
 
 		<?php include("footer.php"); ?>
+
+		<div id="chat-bubble">
+			<!-- A fake / hidden checkbox is used as click reciever,so you can use the :checked selector on it. -->
+			<input type="checkbox" />
+			<!-- Some spans to act as a hamburger. They are acting like a real hamburger, not that McDonalds stuff. -->
+			<picture>
+				
+				<?php include("images/chat-bubble.php"); ?>
+				<?php include("images/chat-close.php"); ?>
+			</picture>
+			<!-- Too bad the menu has to be inside of the button but hey, it's pure CSS magic. -->
+			
+			<div id="chat-window">
+				
+				<div id="chat-title">
+					<h3>Chat Window</h3>
+				</div>
+				
+				<div id="chat-message">
+					<p>Start message here...</p>
+				</div>
+			</div>
+
+			<div id="support">
+				
+				<h3>Still have questions?</h3>
+
+				<p>Chat with one of our experts - available 24/7! →</p>
+			</div>
+		</div>
 	</body>
 </html>
