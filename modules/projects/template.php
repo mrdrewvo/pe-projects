8b4ba54aa60feb6f3605f4c67cd1273bb78c47ed
Drew Vo <andrew.d.vo@gmail.com>
2023-02-07 15:16:35 -0500

    reformatted projects page to call on data array

--- modules/projects/template.php
+++ modules/projects/template.php
@@ -1,47 +1,27 @@
+<?php include("data/projects-data.php"); ?>
+
 <section class="projects">
-
 	<inner-column>
-		
 		<featured-projects-title>
-
 			<h2>Featured Projects</h2>
 			
-			<h2>I made these!</h2>
+			<h2>My latest work!</h2>
 		</featured-projects-title>
 
-		<featured-projects-mod>
-
-			<ul class="featured-projects-list">
-				
+		<ul class="featured-projects-list">
+			<?php foreach($projectsData as $project) { ?>
 				<li>
-					<a class="project-module" href="projects/theme-challenge" target="_blank" rel="noopener noreferrer">
-						
-						<h3>Industry Theme Challenge</h3>
-						
-						<p>A study in visual stylings for the project management tool industry</p>
-					</a>
-				</li>
+					<a class="project-module" href="projects/<?=$project['link']?>" target="_blank" rel="noopener noreferrer">
+						<picture>
+							<img src="images/project-thumbnails/<?=$project['link']?>.jpg" alt="">
+						</picture>
+								
+						<h3><?=$project["title"]?></h3>
 
-				<li>
-
-					<a class="project-module" href="projects/doordash-copy" target="_blank" rel="noopener noreferrer">
-						
-						<h3>DoorDash Custom Copy</h3>
-						
-						<p>Emulated merchant sign-up page for copy customization trials</p>
+						<p><?=$project["description"]?></p>
 					</a>
-				</li>
-
-				<li>
-					
-					<a class="project-module" href="projects/cake-design-by-gregory" target="_blank" rel="noopener noreferrer">
-					
-						<h3>Cake Design by Gregory</h3>
-						
-						<p>Personal website for sugar flower artist, Gregory. (Hint: look for those hidden links!)</p>
-					</a>
-				</li>
-			</ul>
-		</featured-projects-mod>
+				</li>							
+			<?php } ?>
+		</ul>
 	</inner-column>
 </section>
