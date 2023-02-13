d26a024cc5f951f48cfccc389fa7c622b29ffa54
Drew Vo <andrew.d.vo@gmail.com>
2023-02-13 17:24:43 -0500

    added bottom margin on projects page

--- router.php
+++ router.php
@@ -4,10 +4,8 @@
 	$page = null;
   
 	if ( isset($_GET["page"]) ) {
-  
-   	$page = $_GET["page"]; //url?page=string
-	} else {
-      
+	   	$page = $_GET["page"]; //url?page=string
+	} else { 
 		$page = "welcome"; //default
 	}
 
