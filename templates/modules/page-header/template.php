70668c0064a912c231b2e87f178f8bed143eeaab
Drew Vo <andrew.d.vo@gmail.com>
2023-02-19 13:50:39 -0800

    created page-header

---
+++ templates/modules/page-header/template.php
@@ -0,0 +1,12 @@
+<?php
+	$title1 = $pageData["title1"] ?? "This is the page-header title";
+	$title2 = $pageData["title2"] ?? "Many pages will utilize this <code>page-header</code> module. It includes the main page title. and a supporting pargaraph to get the visitor aquinted with the page's goals";
+?>
+
+<header class='page-header'>
+	<inner-column>
+		<h1><?=$title1?></h1>
+
+		<h1><?=$title2?></h1>
+	</inner-column>
+</header>
