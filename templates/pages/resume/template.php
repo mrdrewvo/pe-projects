88e07c946287d21fb3650c503d77a5d47dd1754e
Drew Vo <andrew.d.vo@gmail.com>
2023-02-19 21:07:58 -0800

    built resume structure with json file

--- templates/pages/resume/template.php
+++ templates/pages/resume/template.php
@@ -1,77 +1,47 @@
-<section class="resume" id="resume">
-	
-	<inner-column>
+<?php
+	$json = file_get_contents('data/resume-data.json');
+	$resumeData = json_decode($json, true);
+?>
 
-		<resume-mod class="main-section">
+<?php 
+	include('templates/modules/page-header/template.php');
+?>
 
-			<resume-title>
+<?php foreach($resumeData as $section) { ?>
+	<section>
+		<inner-column>
+			<h2 class='attention-voice'><?=$section["heading"]?></h2>
 
-				<h2>Resume</h2>
-				<h2>I did this!</h2>
-			</resume-title>
-
-			<ul class="resume-list">
-					
-				<li class="company">
-					
-					<h3>Thumbtack</h3>
-					<role>
-						
-						<h4>Senior Product Operations Manager</h4>
-						<resume-date>Feb 2022 - Dec 2022</resume-date>	
-					</role>
-				</li>
-					
-				<li class="company">
-					
-					<h3>Verte</h3>
-					<role>
-						
-						<h4>Senior Manager, Customer Success</h4>
-						<resume-date>Aug 2021 - Feb 2022</resume-date>
-					</role>
-				</li>
-
-				<li class="company">
-					
-					<h3>Flexport</h3>
-					<role>
-						
-						<h4>Program Manager, Operational Excellence</h4>
-						<resume-date>Aug 2020 - Aug 2021</resume-date>
-					</role>
-					<role>
-						
-						<h4>Global Operations Manager</h4>
-						<resume-date>Sep 2019 - Aug 2020</resume-date>
-					</role>
-					<role>
-						
-						<h4>Senior Global Operations Associate</h4>
-						<resume-date>Feb 2018 - Sep 2019</resume-date>
-					</role>
-					<role>
-						
-						<h4>Global Operations Associate</h4>
-						<resume-date>Mar 2017 - Feb 2018</resume-date>
-					</role>
-				</li>
-
-				<li class="company">
-					
-					<h3>Shire</h3>
-					<role>
-						
-						<h4>Project Manager</h4>
-						<resume-date>Oct 2015 - Mar 2017</resume-date>
-					</role>
-					<role>
-						
-						<h4>Operations Leadership Development Program Associate</h4>
-						<resume-date>Jul 2013 - Oct 2015</resume-date>
-					</role>
-				</li>
-			</ul>
-		</resume-mod>				
-	</inner-column>
-</section>
+			<ol>
+				<?php foreach($section["experiences"] as $experience) { ?>
+					<li>
+						<?php if($section["heading"] == "Work Experience") { ?>
+							<h3><?=$experience["company"]?></h3>
+							
+							<ul>
+								<?php foreach($experience["details"] as $detail) { ?>
+									<li>
+										<h4><?=$detail["role"]?></h4>
+										<time><?=$detail["year"]?></time>
+										<p><?=$detail["description"]?></p>
+										<!-- <ul>
+											<?php foreach($detail["tools"] as $tool) { ?>
+												<li><?=$tool?></li>
+											<?php } ?>
+										</ul> -->
+									</li>
+								<?php } ?>
+							</ul>
+						<?php } elseif($section["heading"] == "Education") { ?>
+							<li>
+								<h4><?=$experience["degree"]?></h4>
+								<p><?=$experience["institution"]?></p>
+								<time><?=$experience["year"]?></time>
+							</li>
+						<?php } ?>
+					</li>
+				<?php } ?> 
+			</ol>
+		</inner-column>
+	</section>	
+<?php } ?>
