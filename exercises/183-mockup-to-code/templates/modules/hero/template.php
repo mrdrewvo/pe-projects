b4a83edef38c99c19d58a1ffef6bcb19d604a094
Drew Vo <andrew.d.vo@gmail.com>
2023-03-23 15:08:59 -0400

    formatting fixes to hero module

--- exercises/183-mockup-to-code/templates/modules/hero/template.php
+++ exercises/183-mockup-to-code/templates/modules/hero/template.php
@@ -1,17 +1,20 @@
 <?php
 	$title = $section["title"] ?? "Arcu suscipit massa aliquam proin amet";
+	$class = $section["class"] ?? "";
 	$tagline = $section["tagline"] ?? "Luctus felis sit lectus tristique diam ornare bibendum. Arcu auctor suspendisse luctus amet bibendum pellentesque lorem. Malesuada lobortis tristique tortor,";
 	$actions = $section["actions"] ?? ["actions" => ["type" => "link", "text" => "Link", "url" => "?", "style" => "dark"]];
 ?>
 
-<section class="hero-module">
+<section class="hero-module <?=$class?>">
 	<inner-column>
 		<hero-module>
 			<h1><?=$title?></h1>
-			<p><?=$tagline?></p>				
-			<?php foreach($actions as $action) { ?>
-				<?php include("templates/components/$action[type]/template.php");?>
-			<?php } ?>
+			<p><?=$tagline?></p>
+			<action-section>
+				<?php foreach($actions as $action) { ?>
+					<?php include("templates/components/$action[type]/template.php");?>
+				<?php } ?>
+			</action-section>
 		</hero-module>
 	</inner-column>
 </section>
