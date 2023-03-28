e5fe2215d90209960daf584652d1295859d3bba1
Drew Vo <andrew.d.vo@gmail.com>
2023-03-27 23:34:37 -0400

    updated list data file

--- exercises/183-mockup-to-code/templates/components/list-card/template.php
+++ exercises/183-mockup-to-code/templates/components/list-card/template.php
@@ -1,25 +1,8 @@
 <!-- LIST DATA: PROJECT CARD -->
-<?php $slug = $listItem['slug']; ?>
 <?php $title = $listItem["title"]; ?>
-<?php $description = $listItem["description"]; ?>
-<?php $directLink = $listItem["directLink"]; ?>
+<?php $text = $listItem["text"]; ?>
 
-<project-card>
-	<picture>
-		<img src="images/project-thumbnails/<?=$slug?>.jpg" alt="">
-	</picture>
-			
+<list-card>
 	<h3><?=$title?></h3>
-
-	<p><?=$description?></p>
-
-	<div class="project-buttons">
-		<?php if( isset($listItem["caseStudyLink"]) ) { ?>
-			<?php $caseStudyLink = $listItem["caseStudyLink"]; ?>
-			
-			<a class="button" href="<?=$caseStudyLink?>" target="project" rel="noopener noreferrer">Case Study</a>
-		<?php } ?>
-
-		<a class="button" href="<?=$directLink?>" target="project" rel="noopener noreferrer">Direct Link</a>
-	</div>
-</project-card>	
+	<p><?=$text?></p>
+</list-card>	
