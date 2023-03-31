8b1c0eea7e9d1a459dd43ca80c0fcaeafd2d1dcd
Drew Vo <andrew.d.vo@gmail.com>
2023-03-30 22:13:49 -0400

    renamed language component to language-selector

--- exercises/183-mockup-to-code/templates/components/language-selector/template.php
+++ exercises/183-mockup-to-code/templates/components/language-selector/template.php
@@ -1,6 +1,6 @@
 <!-- LINK DATA FROM TEMPLATE DATA FILE -->
 <?php $url = $action["url"]; ?>
-<?php $style = $action["style"]; ?>
+<?php $style = $action["style"] ?? "default-dark"; ?>
 <?php $langImg = $section["langImg"] ?? "https://peprojects.dev/images/square.jpg"; ?>
 <?php $langAbbr = $section["langAbbr"] ?? "EN"; ?>
 
