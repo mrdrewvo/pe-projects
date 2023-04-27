ad6be4e745979c17444d4baed7f27f0816743a9b
Drew Vo <andrew.d.vo@gmail.com>
2023-04-26 21:05:18 -0400

    added lesson 6

--- exercises/197-todo-app/6-lesson.js
+++ exercises/197-todo-app/6-lesson.js
@@ -1,17 +1,91 @@
-class TodoClass {
+// class TodoClass { // aka "syntatic sugar", it's not actually a class - JS doesn't have classes
 
-	constructor() {
-		this.todos = [];
-		this.count = 0;
+// 	constructor() {
+// 		this.todos = [];
+// 		this.count = 0;
+// 	}
+
+// 	add(content) {
+// 		const todo = {
+// 			id: this.count++,
+// 			content,
+// 		};
+// 		this.todos = [...this.todos, todo];
+// 	}
+
+// 	listTodos(todos) {
+// 		this.todos.forEach ( function(todo) {
+// 			console.log(todo);
+// 		});
+// 	}
+// }
+
+// var TodoClass = new TodoClass()
+
+// todoClass.add("Go for a run");
+// todoClass.add("Play tennis");
+// todoClass.listTodos();
+
+class TodoApp {
+
+	constructor(name) {
+		this.name = name;
+		this.data = localStorage;
+		this.idIncrementor = 0,
+		this.todos = this.recallTodos() || [];
+	}
+
+	showUpdate(note = "") {
+		console.log(`--- ${note}`);
+		// console.log('My Todos:', this.todos);
+	}
+
+	saveTodos() {
+		this.data.setItem('todos', JSON.stringify(this.todos));
+	}
+
+	recallTodos() {
+		return JSON.parse(this.data.getItem('todos'));
+	}
+
+	renderTodos() {
+		document.querySelector('main').innerHTML = JSON.stringify(this.todos);
 	}
 
 	add(content) {
 		const todo = {
-			id: this.count++,
+			id: `todo-${++this.idIncrementor}`,
 			content,
-		};
+			};
 		this.todos = [...this.todos, todo];
-	}
+		
+		// access local storage & save this.todos to key "todos" in local storage
+		this.saveTodos();
+		this.showUpdate(`Added ${content}`);
+		this.render();
+	};
 
+	remove(index) {
+		this.showUpdate(`Removed ${this.todos[index].content}`);
+		this.todos.splice(index, 1);		
+	
+		this.saveTodos();
+		this.showUpdate(`Removed ${content}`);
+		this.render();
+	};
 
+	complete(id) {
+		this.todos[id].complete = true;
+
+		this.saveTodos();
+
+		this.showUpdate(`Completed ${this.todos[id].content}`);
+
+		this.render();
+	};
+	
 }
+
+const trial = new TodoApp();
+
+trial.renderTodos();
